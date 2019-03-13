5f77f3c0e13b272f02526d8712f8b8a41ac78aa1
Unknown <lovecoding530@gmail.com>
2019-03-14 02:24:28 +0800

    add versionName to config

--- app/src/main/java/com/coretal/carinspection/fragments/ConfigPrefFragment.java
+++ app/src/main/java/com/coretal/carinspection/fragments/ConfigPrefFragment.java
@@ -1,16 +1,24 @@
 package com.coretal.carinspection.fragments;
 
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.webkit.URLUtil;
 
+import com.coretal.carinspection.BuildConfig;
 import com.coretal.carinspection.R;
 import com.coretal.carinspection.activities.MainActivity;
 import com.coretal.carinspection.preferences.PreferenceFragmentCompat;
 import com.coretal.carinspection.utils.AlertHelper;
 import com.coretal.carinspection.utils.Contents;
+
+import java.util.Locale;
+
+import androidx.preference.PreferenceCategory;
 
 /**
  * Created by Kangtle_R on 1/22/2018.
@@ -35,6 +43,10 @@ public class ConfigPrefFragment extends PreferenceFragmentCompat implements Shar
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.preferences);
         PreferenceManager.getDefaultSharedPreferences(getActivity()).registerOnSharedPreferenceChangeListener(this);
+        PreferenceCategory appCategory = (PreferenceCategory) findPreference("category_app");
+
+        String newTitle = String.format(Locale.US, "App Configuration - v%s(%d)", BuildConfig.VERSION_NAME, BuildConfig.VERSION_CODE);
+        appCategory.setTitle(newTitle);
     }
 
     @Override
